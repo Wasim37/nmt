7179a65faacd9266bd3a5a6733bc89e850d877c3
wasim <wasim37@163.com>
2018-05-12 15:34:57 +0800

    commit

--- nmt/utils/misc_utils.py
+++ nmt/utils/misc_utils.py
@@ -27,6 +27,14 @@ import time
 import numpy as np
 import tensorflow as tf
 
+import io
+import sys
+import urllib.request
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
+res=urllib.request.urlopen('http://www.baidu.com')
+htmlBytes=res.read()
+print(htmlBytes.decode('utf-8'))
+
 
 def check_tensorflow_version():
   min_tf_version = "1.4.0-dev20171024"
